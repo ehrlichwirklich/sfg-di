4078d80f45047a087401317d25ddf8c7902a7edb
ehrlichwirklich <anissa.d.alexandercs@gmail.com>
2020-07-25 14:45:43 -0400

    Di assignment

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Service
 @Profile({"dog", "default"})
 public class DogPetService {
     public String getPetType(){
